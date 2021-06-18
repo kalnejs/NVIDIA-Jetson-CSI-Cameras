35630e6e3efac398386b8cf887f16c9dab9d37cf
klavs <klavs.kalnejs@gmail.com>
2021-06-18 13:27:22 +0300

    dual camera test

--- nvidia_jetson_csi_camera.py
+++ nvidia_jetson_csi_camera.py
@@ -5,38 +5,7 @@ import numpy as np
 import time
 
 
-def gstreamer_pipeline(
-    sensor_id=0,
-    sensor_mode=4,
-    capture_width=1280,
-    capture_height=720,
-    display_width=1280,
-    display_height=720,
-    framerate=30,
-    flip_method=2,
-):
-    return (
-        "nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            sensor_id,
-            sensor_mode,
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-
-class NVIDIA_Jetson_CSI_Camera:
+class NVIDIA_CSI_Camera:
 
     def __init__ (self):
         self.running = False
@@ -46,6 +15,38 @@ class NVIDIA_Jetson_CSI_Camera:
         self.lock = threading.Lock()
         self.has_frame = False
         self.skipped_frames = 0
+
+    @staticmethod
+    def gstreamer_pipeline(
+        sensor_id=0,
+        sensor_mode=4,
+        capture_width=1280,
+        capture_height=720,
+        display_width=1280,
+        display_height=720,
+        framerate=30,
+        flip_method=2,
+    ):
+        return (
+            "nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, "
+            "format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink"
+            % (
+                sensor_id,
+                sensor_mode,
+                capture_width,
+                capture_height,
+                framerate,
+                flip_method,
+                display_width,
+                display_height,
+            )
+        )
 
     def open(self, pipeline, use_pipeline = True):
         if use_pipeline:
@@ -107,9 +108,9 @@ class NVIDIA_Jetson_CSI_Camera:
 
 if __name__ == "__main__":
 
-    camera = NVIDIA_Jetson_CSI_Camera()
+    camera = NVIDIA_CSI_Camera()
     # camera.open(
-    #     gstreamer_pipeline(
+    #     NVIDIA_CSI_Camera.gstreamer_pipeline(
     #         sensor_id=0,
     #         sensor_mode=4,
     #         flip_method=2,
---
+++ nvidia_jetson_stereo_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+from  nvidia_jetson_csi_camera import NVIDIA_CSI_Camera as Camera
+import numpy as np
+
+
+if __name__ == "__main__":
+    camera_left = Camera()
+    camera_right = Camera()
+
+    camera_left.open(
+        Camera.gstreamer_pipeline(
+            sensor_id=0,
+            sensor_mode=4,
+            flip_method=2,
+            display_height=540,
+            display_width=960,
+        )
+    )
+
+    camera_right.open(
+        Camera.gstreamer_pipeline(
+            sensor_id=1,
+            sensor_mode=4,
+            flip_method=2,
+            display_height=540,
+            display_width=960,
+        )
+    )
+
+    cv.namedWindow("Cameras", cv.WINDOW_AUTOSIZE)
+
+    camera_left.start()
+    camera_right.start()
+
+    cTime = 0
+    pTime = 0
+
+    while True:
+
+        _, _, img_left = camera_left.read()
+        _, _, img_right = camera_right.read()
+
+        img = np.hstack((img_left, img_right))
+
+        cv.imshow("Cameras", img)
+
+        if cv.getWindowProperty("Cameras", cv.WINDOW_AUTOSIZE) == -1:
+            break;
+
+        if cv.waitKey(1) & 0xFF == 'q':
+            break
+
+    camera_left.stop()
+    camera_left.release()
+    camera_right.stop()
+    camera_right.release()
+    cv.destroyAllWindows()
